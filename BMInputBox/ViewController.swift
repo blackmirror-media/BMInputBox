9d381da6cbbed77448b7fc2f96d637488f5d0568
Adam Eri <adam.eri@blackmirror.hu>
2015-02-10 13:56:46 +0100

    Init

---
+++ BMInputBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMInputBox
+//
+//  Created by Adam Eri on 10/02/2015.
+//  Copyright (c) 2015 blackmirror media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
